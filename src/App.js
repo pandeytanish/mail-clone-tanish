c5ecde123776fef3aa927783db06e2e5379f5c6d
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-29 13:48:55 +0530

    gmail clone using react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import Main from './Components/main';
+import Body from './Components/Body';
+import Footer from './Components/Footer';
+import Header from './Components/header';
+import Leftside from './Components/leftside';
+import Rightside from './Components/rightside';
+
 
 
 function App() {
   return (
    <>
-   <Main/>
+    <div class="body-wrapper" >
+
+<Header/>
+<Body/>
+<Leftside/>
+<Rightside/>
+<Footer/>
+
+
+</div>
    
    </>
   );
