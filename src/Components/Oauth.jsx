5cf5bebb785551cfc5971aed0e1a1f2aa49833ed
Tanish Pandey <tanishpandey14@gmail.com>
2024-03-16 09:19:47 +0530

    gmail api intigration

--- src/Components/Oauth.jsx
+++ src/Components/Oauth.jsx
@@ -1,27 +1,62 @@
-import { useState } from "react";
-const Oauth = () => { 
-    const [accessToken,setAccessToken]=useState("")
+import { useEffect, useState } from "react";
+const Oauth = () => {
+    const [accessToken, setAccessToken] = useState("")
     const handleLogin = () => {
-     const CLIENT_ID = "686675426695-cqpmmfkuf1cjhah4fq8d851paeo7g69o.apps.googleusercontent.com"
-     const REDIRECT_URI = "http://localhost:3000"
-     const SCOPE = "https://www.googleapis.com/auth/gmail.readonly"
-     const AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token`;
-      window.location.href= AUTH_URL 
+        const CLIENT_ID = "686675426695-cqpmmfkuf1cjhah4fq8d851paeo7g69o.apps.googleusercontent.com"
+        const REDIRECT_URI = "http://localhost:3000"
+        const SCOPE = "https://www.googleapis.com/auth/gmail.readonly"
+        const AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token`;
+        window.location.href = AUTH_URL;
+    };
+    const getAccessToken = () => {
+        const url = window.location.href
+        const token = url.match(/access_token=([^&]+)/)
+        localStorage.setItem("Token",token[1])
+        console.log("Token is", localStorage.getItem("Token"))
+
+    }
+    useEffect(() => (
+        getAccessToken()
+    ), [])
+    const getEmailData = () => {
+        let token = localStorage.getItem("Token")
+        console.log("hello", token)
+        let url = "GET https://gmail.googleapis.com/gmail/v1/users/me/messages"
+        const options = {
+            method : 'GET' ,
+            headers : {
+                'Authorization' : 'Bearer ${token}',
+                'Content-Type':'application/json'
+            }
+        }
+        fetch(url,options)
+        .then(response => response.json())
+        .then(json=>console.log(json))
+        .catch()
+     
     }
 
+
     return (
-        <div className="body-wrapper" >
-        {accessToken ?  (
-          <div> 
-            <h1>Access token</h1>
-            <p>Access Token: {accessToken}</p>
-            </div>) :(
-              <>
-              <button onClick={handleLogin}>Login with google</button>
-              </>
-        )
+
+        <div>
+            {accessToken ? (
+                <div>
+                    <h1>Access token</h1>
+                    <p>Access Token: {accessToken}</p>
+                </div>) : (
+                <>
+                    <button onClick={handleLogin}>Login with google</button>
+                    <button onClick={() => getEmailData()}> Get Emails</button>
+
+
+                </>
+
+            )
+
             }
-      </div>
+
+        </div>
     )
 
 
