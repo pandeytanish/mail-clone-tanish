cc26c4154126e0cb00e07f8555484f136069d3db
Tanish Pandey <tanishpandey14@gmail.com>
2024-03-16 14:17:09 +0530

    fetching messages from the mail

--- src/Components/Oauth.jsx
+++ src/Components/Oauth.jsx
@@ -12,7 +12,7 @@ const Oauth = () => {
         const url = window.location.href
         const token = url.match(/access_token=([^&]+)/)
         localStorage.setItem("Token",token[1])
-        console.log("Token is", localStorage.getItem("Token"))
+        // console.log("Token is", localStorage.getItem("Token"))
 
     }
     useEffect(() => (
@@ -21,21 +21,36 @@ const Oauth = () => {
     const getEmailData = () => {
         let token = localStorage.getItem("Token")
         console.log("hello", token)
-        let url = "GET https://gmail.googleapis.com/gmail/v1/users/me/messages"
+        let url = "https://gmail.googleapis.com/gmail/v1/users/me/messages"
         const options = {
-            method : 'GET' ,
+            method : 'GET',
             headers : {
-                'Authorization' : 'Bearer ${token}',
+                'Authorization' : `Bearer ${token}`,
                 'Content-Type':'application/json'
             }
         }
         fetch(url,options)
         .then(response => response.json())
-        .then(json=>console.log(json))
-        .catch()
-     
+        .then(json=>console.log(fetchMail(json.messages[0].id)))
+        .catch(error=>console.log('Error in fetching mails',error))
     }
 
+const fetchMail = (id) => {
+    // let id = '18e462bcf161bc72'
+    let token = localStorage.getItem("Token")
+    const options = {
+        method : 'GET',
+        headers : {
+            'Authorization' : `Bearer ${token}`,
+            'Content-Type':'application/json'
+        }
+    }
+    let url =  `https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}`
+    fetch(url,options)
+    .then(response => response.json())
+    .then(json=>console.log(json))
+    .catch(error=>console.log('Error in fetching mails',error))
+}
 
     return (
 
@@ -48,6 +63,7 @@ const Oauth = () => {
                 <>
                     <button onClick={handleLogin}>Login with google</button>
                     <button onClick={() => getEmailData()}> Get Emails</button>
+                    <button onClick={() => fetchMail()}> Fetch Emails</button>
 
 
                 </>
